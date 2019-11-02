71d467b31eb089cee39c83f1d24f2feb6933fd59
Christopher Bortel <christopherbortel@Christophers-MacBook-Pro.local>
2019-11-02 15:45:42 -0700

    The Login Form is importing to the LoginPage

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
+import LoginForm from "../../components/LoginForm";
 
 export default class LoginPage extends Component {
     render() {
         return (
             <div>
-                <h1>This </h1>
+            <LoginForm/>
+                <h1>This is LoginPage</h1>
             </div>
         )
-    }
+    };
 }
+// function LoginPage() {
+//     return (
+//         <div>
+//             <LoginForm/>
+//                 <h1>This is LoginPage</h1>
+//             </div>
+//     );
+// }
 
-
-
+// export default LoginPage;
