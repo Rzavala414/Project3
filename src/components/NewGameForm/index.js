0b212286b4a62406f3866180b01913b9dc7dfe02
Christopher Bortel <christopherbortel@Christophers-MacBook-Pro.local>
2019-11-04 13:14:22 -0800

    newgame form is showing inputs, the css grid is not doing its thing

--- src/components/NewGameForm/index.js
+++ src/components/NewGameForm/index.js
@@ -1,54 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
 }));
 
-export default function NewGameForm() {
-  const classes = useStyles();
+export default class NewGameForm extends Component {
+    render() {
+        return (
+            <div class="wrapper">
+                <div class="box a">A</div>
+                <div class="box b">user1</div>
+                <div class="box c">user1avg</div>
+                <div class="box d">user2</div>
+                <div class="box e">user2avg</div>
+                <div class="box f">play</div>
+                <input class="user-one-play"></input>
+                <div class="user-one-play-avg">{}</div>
+                <input class="user-two-play"></input>
+                <div class="user-two-play-avg">{}</div>
+                
+                <input class="user-one-count"></input>
+                <div class="user-one-count-avg">{}</div>
+                <input class="user-two-count"></input>
+                <div class="user-two-count-avg">{}</div>
+                
+                <input class="user-one-crib"></input>
+                <div class="user-one-crib-avg">{}</div>
+                <input class="user-two-crib"></input>
+                <div class="user-two-crib-avg">{}</div>
 
-  function FormRow() {
-    return (
-      <React.Fragment>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>headings</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>item</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>item</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>item</Paper>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+                <input class="user-one-total"></input>
+                <div class="user-one-total-avg">{}</div>
+                <input class="user-two-total"></input>
+                <div class="user-two-total-avg">{}</div>
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-      </Grid>
-    </div>
-  );
+            </div>
+        )
+    }
 }
+
+
+
+    
