6559760eb96034b23e68444cce2ac8f7e7329d80
Eric Cadwell <ericmcadwell@gmail.com>
2019-11-05 14:39:21 -0800

    routing pages and user profile

--- src/components/CreateAccountForm/index.js
+++ src/components/CreateAccountForm/index.js
@@ -1,201 +1,62 @@
-// import React from 'react';
-// import Avatar from '@material-ui/core/Avatar';
-// import Button from '@material-ui/core/Button';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
-// import Box from '@material-ui/core/Box';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-// import Typography from '@material-ui/core/Typography';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Container from '@material-ui/core/Container';
+// import React, { Component } from 'react'
+// import axios from "axios";
+// // import Secret from './components/Secret';
+// // import LoginForm from './components/LoginForm';
+// // import SignUpForm from './components/SignUpForm';
+// // import ManateeForm from './components/ManateeForm';
+// // import Manatee from './components/Manatee';
 
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://material-ui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
+// export default class CreateAccountForm extends Component {
+//   state = {
+//     name:"",
+//     password:"",
+//     loggedInUser:"",
+//     email:"",
+//     // url:"http://localhost:8080",
+//   }
+//   // componentDidMount(){
+//   //   this.readSessions();
+//   //   this.getAllManatees();
+//   // }
 
-// const useStyles = makeStyles(theme => ({
-//   '@global': {
-//     body: {
-//       backgroundColor: theme.palette.common.white,
-//     },
-//   },
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   form: {
-//     width: '100%', // Fix IE 11 issue.
-//     marginTop: theme.spacing(3),
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//   },
-// }));
+//   handleChange= event=>{
+//     const {name,value}=event.target;
+//     this.setState({
+//       [name]:value
+//     })
+//   }
 
-// export default function SignUp() {
-//   const classes = useStyles();
 
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <div className={classes.paper}>
-//         <Avatar className={classes.avatar}>
-//           <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//           Sign up
-//         </Typography>
-//         <form className={classes.form} noValidate>
-//           <Grid container spacing={2}>
-//             <Grid item xs={12} sm={6}>
-//               <TextField
-//                 autoComplete="fname"
-//                 name="firstName"
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 id="firstName"
-//                 label="First Name"
-//                 autoFocus
-//               />
-//             </Grid>
-//             <Grid item xs={12} sm={6}>
-//               <TextField
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 id="lastName"
-//                 label="Last Name"
-//                 name="lastName"
-//                 autoComplete="lname"
-//               />
-//             </Grid>
-//             <Grid item xs={12}>
-//               <TextField
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 id="email"
-//                 label="Email Address"
-//                 name="email"
-//                 autoComplete="email"
-//               />
-//             </Grid>
-//             <Grid item xs={12}>
-//               <TextField
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 name="password"
-//                 label="Password"
-//                 type="password"
-//                 id="password"
-//                 autoComplete="current-password"
-//               />
-//             </Grid>
-//             <Grid item xs={12}>
-//               <FormControlLabel
-//                 control={<Checkbox value="allowExtraEmails" color="primary" />}
-//                 label="I want to receive inspiration, marketing promotions and updates via email."
-//               />
-//             </Grid>
-//           </Grid>
-//           <Button
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             className={classes.submit}
-//           >
-//             Sign Up
-//           </Button>
-//           <Grid container justify="flex-end">
-//             <Grid item>
-//               <Link href="#" variant="body2">
-//                 Already have an account? Sign in
-//               </Link>
-//             </Grid>
-//           </Grid>
-//         </form>
+
+//   handleSignupFormSubmit = event=>{
+//     event.preventDefault();
+//     axios.post('http://localhost:3001/auth/signup',{username:this.state.name,email:this.state.email,password:this.state.password},{withCredentials:true})
+//     .then(res=>{
+//       console.log(res.data,res.status)
+
+//       //add user info to localStorage
+
+//       //redirect to the dashboard
+//       // this.props.history.push("/Dashboard")
+//       this.handleLoginFormSubmit();
+//     }).catch(err=>{
+//       console.log(err.response);
+//     })
+//   }
+
+
+//   render() {
+//     return (
+//       <div>
+//   <h3>Signup</h3>
+//             <form>
+//                 <input placeholder="Username" name="name" value={this.state.name} onChange={this.handleChange} />
+//                 <input placeholder="Password" name="password" value={this.state.password} type="password" onChange={this.handleChange} />
+//                 <input placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} />
+//                 <input type="submit" onClick={this.handleSignupFormSubmit} />
+//             </form>
+//         <button onClick={this.readSessions}>Readsessions</button>
 //       </div>
-//       <Box mt={5}>
-//         <Copyright />
-//       </Box>
-//     </Container>
-//   );
+//     )
+//   }
 // }
-import React, { Component } from 'react'
-import axios from "axios";
-// import Secret from './components/Secret';
-// import LoginForm from './components/LoginForm';
-// import SignUpForm from './components/SignUpForm';
-// import ManateeForm from './components/ManateeForm';
-// import Manatee from './components/Manatee';
-
-export default class CreateAccountForm extends Component {
-  state = {
-    name:"",
-    password:"",
-    loggedInUser:"",
-    email:"",
-    // url:"http://localhost:8080",
-  }
-  // componentDidMount(){
-  //   this.readSessions();
-  //   this.getAllManatees();
-  // }
-
-  handleChange= event=>{
-    const {name,value}=event.target;
-    this.setState({
-      [name]:value
-    })
-  }
-
-
-
-  handleSignupFormSubmit = event=>{
-    event.preventDefault();
-    axios.post('/signup',{username:this.state.name,email:this.state.email,password:this.state.password},{withCredentials:true}).then(res=>{
-      console.log(res.data,res.status)
-      this.handleLoginFormSubmit();
-    }).catch(err=>{
-      console.log(err.response);
-    })
-  }
-
-
-  render() {
-    return (
-      <div>
-  <h3>Signup</h3>
-            <form>
-                <input name="name" value={this.state.name} onChange={this.handleChange} />
-                <input name="password" value={this.state.password} type="password" onChange={this.handleChange} />
-                <input name="email" value={this.state.email} onChange={this.handleChange} />
-                <input type="submit" onClick={this.handleSignupFormSubmit} />
-            </form>
-        <button onClick={this.readSessions}>Readsessions</button>
-      </div>
-    )
-  }
-}
