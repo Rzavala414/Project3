7ae10087b11fe312f7a04a5b5a6e837a3eb3b208
Eric Cadwell <ericmcadwell@gmail.com>
2019-11-06 13:48:52 -0800

    user data displays on to the profile page

--- src/components/UserStats/index.js
+++ src/components/UserStats/index.js
@@ -5,7 +5,6 @@ const backendUrl = "http://localhost:3001";
 export default class UserStats extends Component {
     state = {
         username: "",
-        // instead of wins and losses we need win percentage and games played
         wins: "",
         losses: "",
         playAvg: "",
@@ -13,7 +12,7 @@ export default class UserStats extends Component {
         cribAvg: "",
         skunks: "",
         skunked: "",
-        games: "",
+        games: [],
     };
 
     handleChange = event => {
@@ -27,11 +26,21 @@ export default class UserStats extends Component {
 // check to see if user info is in local storage
 // if no, redirect to login, otherwise:
 // parse user object and set the state for all of the state variables
-        let userData = {wins:this.state.wins,losses:this.state.losses,playAvg:this.state.playAvg,countAvg:this.state.countAvg,cribAvg:this.state.cribAvg,skunks:this.state.skunks,skunked:this.state.skunked}
-        axios
-            .get(backendUrl+"/api/profile", { withCredentials: true })
+        axios.get(backendUrl+"/api/profile", { withCredentials: true })
             .then(data=>{
+                this.setState({
+                    username: data.data.username,
+                    wins: data.data.wins,
+                    losses: data.data.losses,
+                    playAvg: data.data.playAvg,
+                    countAvg: data.data.countAvg,
+                    cribAvg: data.data.cribAvg,
+                    skunks: data.data.skunks,
+                    skunked: data.data.skunked,
+                    games: data.data.games,
 
+                })
+                console.log("games"+data.data.games.length)
                 console.log("THIS IS RESPONSE",data)
 
             }).catch(err=>console.log("THIS IS ERROR",err))
@@ -47,7 +56,7 @@ export default class UserStats extends Component {
                 <div className="wrapper">
                     <div className="box1">
                         <p className="winPerc" onChange={this.handleChange}>Win Percentage: {this.state.wins}</p>
-                        <p className="gamesPlayed" onChange={this.handleChange}>Games Played: {this.state.user}</p>
+                        <p className="gamesPlayed" onChange={this.handleChange}>Games Played: {this.state.games.length}</p>
                     </div>
                     <div className="box2">
                         <p className="playAvg" onChange={this.handleChange}>Play Average: {this.state.playAvg}</p>
