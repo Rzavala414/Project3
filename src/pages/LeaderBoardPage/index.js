6368a1ca0875ae1a86f5760f739bb0b2119274f6
Roger Zavala <Rogeliozavala15@gmail.com>
2019-11-08 14:14:20 -0800

    added border to leader boards

--- src/pages/LeaderBoardPage/index.js
+++ src/pages/LeaderBoardPage/index.js
@@ -6,7 +6,9 @@ export default class LeaderboardPage extends Component {
     render() {
         return (
             <div>
-                <h1>This is LeaderBoardPage</h1>
+                <div>
+                <h1>Leader board Page </h1>
+                </div>
                 <GlobalStatsTable />
             </div>
         )
