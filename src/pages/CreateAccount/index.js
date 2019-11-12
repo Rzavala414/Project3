287e60335fdcf37355f26a0c8904614904473d88
danlgrigg <griggdl@gmail.com>
2019-11-12 10:11:24 -0800

    deploy2

--- src/pages/CreateAccount/index.js
+++ src/pages/CreateAccount/index.js
@@ -8,8 +8,8 @@ export default class CreateAccount extends Component {
         password: "",
         loggedInUser: "",
         email: "",
-        url:"http://localhost:3001",
-        // url:" https://cribsmack-be.herokuapp.com/ ",
+        // url:"http://localhost:3001",
+        url:" https://cribsmack-be.herokuapp.com/ ",
     }
     // componentDidMount(){
     //   this.readSessions();
@@ -27,8 +27,8 @@ export default class CreateAccount extends Component {
 
     handleSignupFormSubmit = event => {
         event.preventDefault();
-        axios.post('http://localhost:3001/auth/signup', { username: this.state.name, email: this.state.email, password: this.state.password }, { withCredentials: true })
-        // axios.post(' https://cribsmack-be.herokuapp.com/ /auth/signup', { username: this.state.name, email: this.state.email, password: this.state.password }, { withCredentials: true })
+        // axios.post('http://localhost:3001/auth/signup', { username: this.state.name, email: this.state.email, password: this.state.password }, { withCredentials: true })
+        axios.post(' https://cribsmack-be.herokuapp.com/ /auth/signup', { username: this.state.name, email: this.state.email, password: this.state.password }, { withCredentials: true })
             .then(res => {
                 console.log(res.data, res.status)
 
